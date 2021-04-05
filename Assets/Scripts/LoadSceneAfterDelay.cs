9b801990ce573ee1f2eea9cda05b8fef5fc19d13
NothingFace <nothingface@Hackintosh.local>
2021-04-04 21:18:54 -0700

    initial commit

---
+++ Assets/Scripts/LoadSceneAfterDelay.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadSceneAfterDelay : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (loadSceneAfterDelay (1));
+	}
+
+	IEnumerator loadSceneAfterDelay(float waitbySecs){
+		yield return new WaitForSeconds(waitbySecs);
+		Application.LoadLevel (1);
+	} 
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# rudolf-ar
-Augmented Reality app built in Unity
+# Rudolf AR
+
+Augmented Reality app built in Unity and coded using C#. 3D assets and particles created by myself. Video created by Matt Siditsky.
